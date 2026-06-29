911b74072a1947851eb044cbe02631dde917d861
Muhammad Irfan Tijanie bin Mohammad Tasnim <adm_irfant@it-kl.dug.com>
2026-06-29 18:25:52 +0800

    add remove ftrace lock clause

--- io_exporter.sh
+++ io_exporter.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -f /var/tmp/.ftrace-lock
 /opt/io_exporter/iosnoop -ts 10 > /tmp/io_exporter/iosnoop.raw
 /opt/io_exporter/iosnoop_processing.sh /tmp/io_exporter/iosnoop.raw > /tmp/io_exporter/iosnoop.json
 
